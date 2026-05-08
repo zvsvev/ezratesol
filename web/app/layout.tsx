7c0eda8c481c5ef536bffbfbcd324bc556ea62a7
zvsvev <muhammadulinnuha2003@mail.ugm.ac.id>
2026-05-08 13:40:59 +0700

    Refine the UI

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import './globals.css'
 import ContextProvider from '@/context'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'EZRATE',
@@ -13,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <ContextProvider>{children}</ContextProvider>
       </body>
